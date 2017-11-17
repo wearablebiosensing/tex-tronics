efe5d2dcb2c9301a3c02a34eb4be80d91499b966
andrewcpeltier <andrew_peltier@my.uri.edu>
2017-11-17 12:18:28 -0500

    Add files via upload
    11/17/17
    I've made changes to the following:
    Main Activity
    -Uncommented and implemented the BLEModel
    -Made onClickListeners for all remaining buttons
    BluetoothLeService
    -Wrote methods for Disconnect and ReadCharacteristic. Biggest change was the addition of a HashMap for BluetoothGatts so that the disconnect method could access it.
    
    I tried to write almost every comment with my name right before it on every change I made. You can use the find function and type in my name to locate all of my changes.

---
+++ MobileApplications/MainActivity.java
@@ -0,0 +1,108 @@
+package edu.uri.wbl.smartglove;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.UUID;
+
+import edu.uri.wbl.smartglove.ble.models.BluetoothCharacteristicModel;
+import edu.uri.wbl.smartglove.ble.models.BluetoothLeModel;
+import edu.uri.wbl.smartglove.ble.receivers.BleUpdateReceiver;
+import edu.uri.wbl.smartglove.ble.services.BleConnectionService;
+import edu.uri.wbl.smartglove.ble.services.BluetoothLeService;
+
+public class MainActivity extends AppCompatActivity {
+//    private final String BT_ADDR = "F6:D7:BF:73:72:D3";
+    public static final String BT_ADDR = "98:4F:EE:0F:A3:AB";
+
+
+    Context mContext;
+    BluetoothLeModel mBluetoothLeModel;
+
+    // Andrew: Min SDK was changed to 19, then to 21
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContext = this;
+        mBluetoothLeModel = BluetoothLeModel.CREATE(BT_ADDR);
+        registerReceiver(mBleUpdateReceiver, BleUpdateReceiver.INTENT_FILTER);
+
+        Button startButton = (Button) findViewById(R.id.start_btn);
+        Button stopButton = (Button) findViewById(R.id.stop_btn);
+        Button connectButton = (Button) findViewById(R.id.connect_btn);
+        Button disconnectButton = (Button) findViewById(R.id.disconnect_btn);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BluetoothLeService.START(mContext);
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BluetoothLeService.STOP(mContext);
+            }
+        });
+
+        // Andrew: Created connect button click listener
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Uncommented mBluetoothLeModel, put it in here
+                BluetoothLeService.CONNECT(mContext, mBluetoothLeModel);
+            }
+        });
+
+        // Andrew: Created disconnect button click listener
+        disconnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BluetoothLeService.DISCONNECT(mContext, mBluetoothLeModel);
+            }
+        });
+    }
+
+    private BleUpdateReceiver mBleUpdateReceiver = new BleUpdateReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int update = intent.getIntExtra(BleUpdateReceiver.EXTRA_UPDATE, -1);
+            switch(update) {
+                case BleUpdateReceiver.UPDATE_CONNECTED:
+
+                    //BleConnectionService.DISCOVER_SERVICES(mContext, mBluetoothLeModel);
+                    break;
+                case BleUpdateReceiver.UPDATE_DISCONNECTED:
+
+                    break;
+                case BleUpdateReceiver.UPDATE_SERVICES_DISCOVERED:
+                    /*BluetoothLeModel bluetoothLeModel = (BluetoothLeModel) intent.getSerializableExtra(BleUpdateReceiver.EXTRA_DEVICE);
+                    if(bluetoothLeModel == null) {
+                        Log.d("BLE Update Receiver", "Error Retrieving BluetoothLeModel");
+                        return;
+                    }
+                    Log.d(this.getClass().getSimpleName(), "Discovered Services on " + bluetoothLeModel.getBluetoothDeviceAddress());
+                    BluetoothCharacteristicModel bluetoothCharacteristicModel = bluetoothLeModel.getCharacteristic(UUID.fromString("00004003-0000-1000-8000-00805f9b34fb"));
+                    if(bluetoothCharacteristicModel == null) {
+                        Log.d("BLE Update Receiver", "Error finding Characteristic");
+                        return;
+                    }
+                    byte[] value = bluetoothCharacteristicModel.getValue();
+                    Log.d("BLE Update Receiver", value.toString());*/
+                    break;
+                default:
+
+                    break;
+            }
+        }
+    };
+}
