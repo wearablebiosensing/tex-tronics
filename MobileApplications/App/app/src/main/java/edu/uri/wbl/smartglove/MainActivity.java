534d649259fed44138e6708aec7daf72e8720b73
kabuki4774 <kabuki4774@gmail.com>
2017-12-05 08:24:03 -0500

    dont trust

--- MobileApplications/App/app/src/main/java/edu/uri/wbl/smartglove/MainActivity.java
+++ MobileApplications/App/app/src/main/java/edu/uri/wbl/smartglove/MainActivity.java
@@ -16,8 +16,30 @@ import edu.uri.wbl.smartglove.ble.receivers.BleUpdateReceiver;
 import edu.uri.wbl.smartglove.ble.services.BleConnectionService;
 import edu.uri.wbl.smartglove.ble.services.BluetoothLeService;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+
+
+
 public class MainActivity extends AppCompatActivity {
     private final String BT_ADDR = "F6:D7:BF:73:72:D3";
+
+    private final static String SERVER_URI = "tcp://fog.wbl.cloud:1883";
+    private final static String MQTT_TOPIC = "test";//"wbl/smartglove/kiya";
+    private final static String MQTT_TEST_MESSAGE = "Hello from Andy P!";
+    private final static String CLIENT_BASE_ID = "smartglove";
+
+    private MqttAndroidClient mMqttAndroidClient;
+    private String mClientId;
 
     Context mContext;
     //BluetoothLeModel mBluetoothLeModel;
@@ -49,6 +71,27 @@ public class MainActivity extends AppCompatActivity {
                 BluetoothLeService.STOP(mContext);
             }
         });
+
+        // Initialize MQTT Client
+        mClientId = CLIENT_BASE_ID + System.currentTimeMillis();    // Client ID must be unique
+        mMqttAndroidClient = new MqttAndroidClient(mContext, SERVER_URI, mClientId);
+        mMqttAndroidClient.setCallback(mMqttCallbackExtended);
+
+        // Setup MQTT Connection Options
+        /**
+         * TODO:
+         *      Verify and finalize the MQTT Connection Options
+         */
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+
+        // Attempt to connect to Broker
+        try {
+            mMqttAndroidClient.connect(mqttConnectOptions, null, mIMqttActionListener);
+        } catch (MqttException e) {
+            Log.w("MQTT Demo", e.getMessage());
+        }
     }
 
     private BleUpdateReceiver mBleUpdateReceiver = new BleUpdateReceiver() {
@@ -82,6 +125,54 @@ public class MainActivity extends AppCompatActivity {
 
                     break;
             }
+        }
+    };
+
+    public void publishMessage(String messageText){
+        try {
+            MqttMessage message = new MqttMessage();
+            message.setPayload(messageText.getBytes());
+            mMqttAndroidClient.publish(MQTT_TOPIC, message);
+            if(!mMqttAndroidClient.isConnected()){
+                Log.w(this.getClass().getSimpleName(), "MQTT Not Connected! Message has been buffered");
+            }
+        } catch (MqttException e) {
+            Log.e(this.getClass().getSimpleName(),"Error Publishing: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private IMqttActionListener mIMqttActionListener = new IMqttActionListener() {
+        @Override
+        public void onSuccess(IMqttToken asyncActionToken) {
+            Log.d("MQTT Listener", "Action Success");
+        }
+
+        @Override
+        public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+            Log.d("MQTT Listener", "Action Failure");
+        }
+    };
+
+    private MqttCallbackExtended mMqttCallbackExtended = new MqttCallbackExtended() {
+        @Override
+        public void connectComplete(boolean reconnect, String serverURI) {
+            Log.d("MQTT Callback", "Connect Complete");
+        }
+
+        @Override
+        public void connectionLost(Throwable cause) {
+            Log.d("MQTT Callback", "Connection Lost");
+        }
+
+        @Override
+        public void messageArrived(String topic, MqttMessage message) throws Exception {
+            Log.d("MQTT Callback", "Message Received");
+        }
+
+        @Override
+        public void deliveryComplete(IMqttDeliveryToken token) {
+            Log.d("MQTT Callback", "Delivery Complete");
         }
     };
 }
