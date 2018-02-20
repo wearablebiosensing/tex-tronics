1903a51485687375cd68b40ed03100aafba16d99
Andrew <Andrew@212.69.20.172.s.wireless.uri.edu>
2018-02-20 10:19:11 -0500

    Fixed Obscenities

---
+++ App(2-20-2018)/app/src/main/java/edu/uri/wbl/tex_tronics/smartglove/smart_glove/SmartGloveState.java
@@ -0,0 +1,9 @@
+package edu.uri.wbl.tex_tronics.smartglove.smart_glove;
+
+/**
+ * Created by mcons on 2/12/2018.
+ */
+
+public enum SmartGloveState {
+    NOT_SELECTED, DISCONNECTED, CONNECTED, STREAMING
+}
