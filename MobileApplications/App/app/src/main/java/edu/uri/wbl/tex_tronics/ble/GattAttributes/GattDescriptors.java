05720fc6e582a32267006a8164f17dc76df312a5
Andrew <Andrew@dyn147.ele.uri.edu>
2018-03-28 13:58:43 -0400

    Deleted a Buncha Stuff
    ;)

--- MobileApplications/App/app/src/main/java/edu/uri/wbl/tex_tronics/ble/GattAttributes/GattDescriptors.java
+++
@@ -1,9 +0,0 @@
-package edu.uri.wbl.tex_tronics.ble.GattAttributes;
-
-/**
- * Created by mcons on 12/4/2017.
- */
-
-public class GattDescriptors {
-    public static final String NOTIFICATION_DESCRIPTOR = "00002902-0000-1000-8000-00805f9b34fb";
-}
